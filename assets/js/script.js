37bae8c82d263fa8d25626ca2b12b3d09d196696
G-Prasath <82494750+G-Prasath@users.noreply.github.com>
2023-05-22 12:51:42 +0530

    Hero slide Done

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,6 @@ let navbar = document.querySelector(".navbar");
 let searchBox = document.querySelector(".search-box .bx-search");
 // let searchBoxCancel = document.querySelector(".search-box .bx-x");
 
-    // searchBox.addEventListener("click", ()=>{
-    //   navbar.classList.toggle("showInput");
-    //   if(navbar.classList.contains("showInput")){
-    //     searchBox.classList.replace("bx-search" ,"bx-x");
-    //   }else {
-    //     searchBox.classList.replace("bx-x" ,"bx-search");
-    //   }
-    // });
-
 // sidebar open close js code
 let navLinks = document.querySelector(".nav-links");
 let menuOpenBtn = document.querySelector(".navbar .bx-menu");
@@ -23,7 +14,6 @@ navLinks.style.left = "0";
 menuCloseBtn.onclick = function() {
 navLinks.style.left = "-100%";
 }
-
 
 // sidebar submenu open close js code
 let htmlcssArrow = document.querySelector(".htmlcss-arrow");
--- index.php
+++ index.php
@@ -6,78 +6,145 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Civil Construction</title>
     <link rel="stylesheet" href="assets/css/style.css" />
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
+
     
 </head>
 <body>
-    <!--------- Top Header ------------>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12 p-0">
-                    <div class="top-header">
-                        <div class="header-email">
-                            <a href="#"><img src="assets/images/header-icons/mail.png" alt="">Exmple@gmail.com</a>
-                        </div>
-                        <span>|</span>
-                        <div class="header-phone">
-                            <a href="#"><img src="assets/images/header-icons/phone.png" alt="">+98548 658 125</a>
-                        </div>
-                        <span>|</span>
-                        <div class="header-social">
-                            <a href="#"><img src="assets/images/header-icons/twitter.png" alt="" /></a>
-                            <a href="#"><img src="assets/images/header-icons/facebook.png" alt="" /></a>
-                            <a href="#"><img src="assets/images/header-icons/instagram.png" alt="" /></a>
-                            <a href="#"><img src="assets/images/header-icons/whatsapp.png" alt="" /></a>
-                            <a href="#"><img src="assets/images/header-icons/youtube.png" alt="" /></a>
-                        </div>
+<!--------- Top Header ------------>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 p-0">
+                <div class="top-header">
+                    <div class="header-email">
+                        <a href="#"><img src="assets/images/header-icons/mail.png" alt="">Exmple@gmail.com</a>
+                    </div>
+                    <span>|</span>
+                    <div class="header-phone">
+                        <a href="#"><img src="assets/images/header-icons/phone.png" alt="">+98548 658 125</a>
+                    </div>
+                    <span>|</span>
+                    <div class="header-social">
+                        <a href="#"><img src="assets/images/header-icons/twitter.png" alt="" /></a>
+                        <a href="#"><img src="assets/images/header-icons/facebook.png" alt="" /></a>
+                        <a href="#"><img src="assets/images/header-icons/instagram.png" alt="" /></a>
+                        <a href="#"><img src="assets/images/header-icons/whatsapp.png" alt="" /></a>
+                        <a href="#"><img src="assets/images/header-icons/youtube.png" alt="" /></a>
                     </div>
                 </div>
             </div>
         </div>
-    <!--------- Top Header ------------>
+    </div>
+<!--------- Top Header ------------>
 
-    <!-- ----------- Start Navbar ----------->
-<header>
-    <nav>
-        <div class="hero-logo">
-            <img class="img-fluid" src="assets/images/logo-1.png" alt="">
+<!-- ----------- Start Navbar ----------->
+<nav>
+  <div class="navbar">
+    <i class='bx bx-menu'></i>
+    <div class="logo"><a href="#"><img src="assets/images/logo-1.png" alt=""></a></div>
+    <div class="nav-links">
+      <div class="sidebar-logo">
+        <div class="logo-name"><a href="#"><img src="assets/images/logo-1.png" alt=""></a></div>
+        <i class='bx bx-x' ></i>
+      </div>
+      <ul class="links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li>
+          <a href="#">Residential</a>
+          <i class='bx bxs-chevron-down htmlcss-arrow arrow'></i>
+          <ul class="htmlCss-sub-menu sub-menu">
+            <li><a href="#">House</a></li>
+            <li><a href="#">Flats</a></li>
+            <li><a href="#">Condominium</a></li>
+            <li><a href="#">Duplex</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">Industrial</a>
+          <i class='bx bxs-chevron-down js-arrow arrow'></i>
+          <ul class="js-sub-menu sub-menu">
+            <li><a href="#">Cold Storage Buildings</a></li>
+            <li><a href="#">Data Centers</a></li>
+            <li><a href="#">Manufacturing Buildings</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">Commercial</a>
+          <i class='bx bxs-chevron-down tst-arrow arrow'></i>
+          <ul class="tst-sub-menu sub-menu">
+            <li><a href="#">Hospitals</a></li>
+            <li><a href="#">Shopping complexes</a></li>
+            <li><a href="#">Office</a></li>
+            <li><a href="#">Warehouses</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- ----------- End Navbar ---------- -->
+
+<!-- -------------- Start Hero Slide ----------->
+
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" data-interval="2500">
+  <div class="carousel-inner">
+
+    <div class="carousel-item active">
+      <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 p-0">
+                <div class="bg-img-1">
+                    <div class="first-slide">
+                        <h1>Dream House</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo dolores pariatur nam esse voluptatem voluptatum non similique corporis?</p>
+                        <button class="cover-btn"><a href="#">Read More</a></button>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
-        <!-- <div class="nav-menu">
-            <img src="assets/images/nav-menu.png" class="img-fluid" alt="">
-        </div> -->
-        <ul class="menu">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Residential</a>
-                <ul class="sub-menu">
-                    <li><a href="#">House</a></li>
-                    <li><a href="#">Flats</a></li>
-                    <li><a href="#">Condominium</a></li>
-                    <li><a href="#">Duplex</a></li>
-                </ul>
-            </li>
-            <li><a href="#">Industrial</a>
-                <ul class="sub-menu">
-                    <li><a href="#">Hospitals</a></li>
-                    <li><a href="#">Shopping complexes</a></li>
-                    <li><a href="#">Office</a></li>
-                    <li><a href="#">Warehouses</a></li>
-                </ul>
-            </li>
-            <li><a href="#">Commercial</a>
-                <ul class="sub-menu">
-                    <li><a href="#">Cloude Storage Buildings</a></li>
-                    <li><a href="#">Data Science</a></li>
-                    <li><a href="#">Manufacturing Buildings</a></li>
-                </ul>
-            </li>
-            <li><a href="#">Contact Us</a></li>
-        </ul>
-    </nav>
-</header>
-    <!-- ----------- End Navbar ---------- -->
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 p-0">
+                <div class="bg-img-1">
+                    <div class="second-slide">
+                        <h1>Dream House</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo dolores pariatur nam esse voluptatem voluptatum non similique corporis?</p>
+                        <button class="cover-btn"><a href="#">Read More</a></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 p-0">
+                <div class="bg-img-1">
+                    <div class="thired-slide">
+                        <h1>Dream House</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo dolores pariatur nam esse voluptatem voluptatum non similique corporis?</p>
+                        <button class="cover-btn"><a href="#">Read More</a></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<!-- -------------- End Hero Slide ----------->
 
 
 
@@ -91,7 +158,10 @@
 
 
 
-    <script src="assets/js/script.js"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/script.js"></script>
+
 </body>
 </html>
