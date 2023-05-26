b252946a726c0938777a9dc099206c9f0df93b72
G-Prasath <guruprasathmsc@gmail.com>
2023-05-26 12:37:28 +0530

    Sample Test

--- nav.php
+++ nav.php
@@ -18,15 +18,7 @@
 
 
    
-      <div class="box">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <div class="content">
-          <h2>My animated Border </h2>
-          <p><a>All our modules are designed to play nicely with responsive of course. At the end of the day it comes down to the                 theme you use - our code doesn't get in your way.</a></p>
-        </div>
+
         
       </div>
 
