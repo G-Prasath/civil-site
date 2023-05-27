627f6c14ecbc8d65eea3d00cd32ff032af69a93f
G-Prasath <guruprasathmsc@gmail.com>
2023-05-27 13:55:41 +0530

    Home Done with Layout

--- index.php
+++ index.php
@@ -492,7 +492,7 @@
           </div>
         </div>
         <div class="faq-video">
-          <iframe width="550" height="290" src="https://www.youtube.com/embed/Yu9Xh8HJIbQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe src="https://www.youtube.com/embed/Yu9Xh8HJIbQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
       </div>
     </div>
@@ -693,13 +693,10 @@
     </div>
   </div>
 </div>
-
 <!-------------- end Fun Fact ----------->
-
 </div>
 
 <!-- ============== Start CTA Section ===============-->
-
 <div id="cta-sec">
   <div class="container-fluid">
     <div class="row">
@@ -717,12 +714,108 @@
     </div>
   </div>
 </div>
-
-
-
-
-
 <!-- ============== End CTA Section ==============-->
+<!-- ============== Start Footer Section ==============-->
+<div id="footer">
+  <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="footer-sec">
+            <ul>
+              <li>
+                <div class="f-img">
+                  <img src="assets/images/logo-1.png" class="img-fluid" alt="">
+                </div>
+                <div class="f-cnt">
+                  <p>Mekark company provides residential and commercial clients across the world with innovative and cost-effective builds.</p>
+                </div>
+                <div class="f-social">
+                  <ul>
+                    <li><a href="#"><i class='bx bxl-facebook'></i></a></li>
+                    <li><a href="#"><i class='bx bxl-twitter'></i></a></li>
+                    <li><a href="#"><i class='bx bxl-linkedin'></i></a></li>
+                    <li><a href="#"><i class='bx bxl-pinterest-alt'></i></a></li>
+                    <li><a href="#"><i class='bx bxl-instagram'></i></a></li>
+                    <li><a href="#"><i class='bx bxl-youtube'></i></a></li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <div class="f-head">
+                  <h6>Usefull Links</h6>
+                </div>
+                <div class="f-links">
+                  <ul>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Our Products</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Career</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <div class="f-head">
+                  <h6>Quick Links</h6>
+                </div>
+                <div class="f-links">
+                  <ul>
+                      <li><a href="#">About Us</a></li>
+                      <li><a href="#">Our Products</a></li>
+                      <li><a href="#">Blog</a></li>
+                      <li><a href="#">Career</a></li>
+                      <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+              </li>
+              <li>
+                <div class="f-head">
+                  <h6>Our Address</h6>
+                </div>
+                <div class="f-addr">
+                  <ul>
+                    <li>
+                      <p>15,62nd street, Ashok Nagar, Chennai-600083</p>
+                    </li>
+                    <li>
+                      Phone : <a href="tel:9790924754">+91 97909 24754</a>
+                    </li>
+                    <li>
+                      Email : <a href="mailto:admin@mekark.com">admin@mekark.com</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+  </div>
+</div>
+<!-- ============== End Footer Section ==============-->
+<!-- ============== Start Mini Footer Section ============-->
+<div id="cp-rights">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12 p-0">
+        <div class="cp-cnt">
+          <ul>
+            <li>
+              <p>Copyright © 2023 Mekark | Civil Construction</p>
+            </li>
+            <li>
+              <a href="javascript:;">Privace & Policy</a><span></span>
+              <a href="javascript:;">Terms</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- ============== End Mini Footer Section ==============-->
+
+
 
 
 
